4e38e9bfe00afd7efe6bf675354699a2b4dea7f6
Romain-Brement <rbrement@keyneosoft.fr>
2023-10-19 11:25:45 +0200

    test config image

--- emile-magique/next.config.js
+++ emile-magique/next.config.js
@@ -5,6 +5,9 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   assetPrefix: isProd ? "https://lamobilery.github.io/emile-magique/" : undefined,
+  images: {
+    domains: ["https://lamobilery.github.io/emile-magique/"],
+  },
 };
 
 module.exports = nextConfig;
