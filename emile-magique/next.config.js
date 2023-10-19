2e50d7d2c0f8fb6e37ace359db5d03d56b8f8b55
Romain-Brement <rbrement@keyneosoft.fr>
2023-10-19 11:21:35 +0200

    fix: images config

--- emile-magique/next.config.js
+++ emile-magique/next.config.js
@@ -5,9 +5,6 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   output: "export",
   assetPrefix: isProd ? "https://lamobilery.github.io/emile-magique/" : undefined,
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
