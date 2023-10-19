5307719567ede116c775f3c7e6c848a26bc57ce1
Romain-Brement <rbrement@keyneosoft.fr>
2023-10-19 11:12:00 +0200

    test config asset

--- emile-magique/next.config.js
+++ emile-magique/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
+  assetPrefix: isProd ? "https://lamobilery.github.io/emile-magique/" : undefined,
   images: {
     unoptimized: true,
   },
