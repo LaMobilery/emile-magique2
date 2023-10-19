d094f82939ec7ebb24d9b557ac128c429b9e27bb
Romain-Brement <rbrement@keyneosoft.fr>
2023-10-19 11:00:19 +0200

    update config

--- emile-magique/next.config.js
+++ emile-magique/next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   output: "export",
-  basePath: "/",
-  experimental: {
-    appDir: true,
-  },
   images: {
     unoptimized: true,
   },
