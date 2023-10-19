337deb3878e7a47864565095588dc1d20f4f45ba
Romain-Brement <rbrement@keyneosoft.fr>
2023-10-19 09:58:13 +0200

    congif nextjs

--- emile-magique/next.config.js
+++ emile-magique/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
+  basePath: "/gh-pages",
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
