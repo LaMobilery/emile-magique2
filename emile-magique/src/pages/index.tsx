5680ff965b0b89c9407b92ac0a17cec58ffa41ec
Romain-Brement <rbrement@keyneosoft.fr>
2023-10-19 14:36:46 +0200

    create page

---
+++ emile-magique/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className="red">
+      <div className={styles.center}>
+        <Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
+      </div>
+    </main>
+  );
+}
